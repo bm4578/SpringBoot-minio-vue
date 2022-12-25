41d7d19ba6d2aa0f90457035faeba6d368fea4c6
codetype <m15319631269@163.com>
2022-12-25 15:39:47 +0800

    debug

--- src/router/index.js
+++ src/router/index.js
@@ -27,16 +27,6 @@ const routes = [
       }
     ]
   }
-  , {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-    }
-  }
 ]
 
 const router = new VueRouter({
